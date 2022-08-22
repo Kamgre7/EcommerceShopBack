abb3dc6675498f8fef56f9c50baf9b4103563226
kamgre7@gmail.com <kamgre7@gmail.com>
2022-08-22 21:23:41 +0200

    added DB mysql configuration

---
+++ src/database/datasource.ts
@@ -0,0 +1,32 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+
+export const connectionSource = new DataSource({
+  type: 'mysql',
+  //host: 'localhost',
+  host: process.env.DB_HOST,
+  //port: 3306,
+  port: parseInt(process.env.DB_PORT, 10),
+  //username: 'root',
+  username: process.env.DB_USERNAME,
+  //password: '',
+  password: process.env.DB_PASSWORD,
+  //database: 'megak_ecommerce',
+  database: process.env.DB_DATABASE,
+  entities: [
+    'dist/**/**/**/**.entity{.ts,.js}',
+    'dist/**/**/**.entity{.ts,.js}',
+    'dist/**/**.entity{.ts,.js}',
+  ],
+  bigNumberStrings: false,
+  logging: true,
+  migrations: ['dist/database/migrations/*.js'],
+  synchronize: true,
+  autoLoadEntities: true,
+  extra: {
+    decimalNumbers: true,
+  },
+  cli: {
+    migrationsDir: 'migrations',
+  },
+} as DataSourceOptions);
