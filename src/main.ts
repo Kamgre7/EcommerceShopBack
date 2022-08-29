e07e44b1214344f8dc9cceded22996cffc552d7a
kamgre7@gmail.com <kamgre7@gmail.com>
2022-08-29 22:38:57 +0200

    Added validationPipie, class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,20 @@ async function bootstrap() {
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+
+      whitelist: true,
+      forbidNonWhitelisted: true,
+
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(3001);
 }
