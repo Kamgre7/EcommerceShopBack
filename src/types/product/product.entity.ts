4c63b7dec7f6dee80d5a12ee0ea85d2ea524c539
kamgre7@gmail.com <kamgre7@gmail.com>
2022-09-13 10:21:42 +0200

    Added storage function for Multer and example file - photo. Added types of product

--- src/types/product/product.entity.ts
+++ src/types/product/product.entity.ts
@@ -4,4 +4,21 @@ export interface ProductInterface {
   description: string;
   price: number;
   quantity: number;
+  boughtCount: number;
+  createAt: Date;
+}
+
+export interface MulterDiskUploadedFiles {
+  [fieldname: string]:
+    | {
+        filename: string;
+        size: number;
+        mimetype: string;
+        originalname: string;
+        fieldname: string;
+        encoding: string;
+        destination: string;
+        path: string;
+      }[]
+    | undefined;
 }
