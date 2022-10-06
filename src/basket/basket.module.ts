d2a4cc09715ca6788ff93ca6b1d00470f470a0ad
kamgre7@gmail.com <kamgre7@gmail.com>
2022-10-06 22:23:00 +0200

    Fixed EP /delete User - added remove order history

--- src/basket/basket.module.ts
+++ src/basket/basket.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { BasketController } from './basket.controller';
 import { ProductModule } from '../product/product.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [forwardRef(() => ProductModule)],
+  imports: [forwardRef(() => ProductModule), forwardRef(() => UserModule)],
   controllers: [BasketController],
   providers: [BasketService],
   exports: [BasketService],
