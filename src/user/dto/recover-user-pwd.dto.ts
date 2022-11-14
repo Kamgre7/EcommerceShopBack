3f3db5dd976ec1b2d7762398f0bd9d67b04872d9
kamgre7@gmail.com <kamgre7@gmail.com>
2022-11-14 22:58:06 +0100

    Swagger api doc - user module

--- src/user/dto/recover-user-pwd.dto.ts
+++ src/user/dto/recover-user-pwd.dto.ts
@@ -5,14 +5,27 @@ import {
   Length,
   MinLength,
 } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class RecoverUserPwdDto {
   @IsEmail()
   @Length(3, 255)
+  @ApiProperty({
+    type: String,
+    example: 'user@mail.com',
+    minLength: 3,
+    maxLength: 255,
+  })
   email: string;
 
   @IsOptional()
   @IsString()
   @MinLength(6)
+  @ApiPropertyOptional({
+    oneOf: [{ type: 'string' }, { type: 'undefined' }],
+    example: 'userPwd',
+    minLength: 6,
+  })
   pwd: string | undefined;
 }
