dbaa784155bb9fdee70838f4cf7cda1228382a65
kamgre7@gmail.com <kamgre7@gmail.com>
2022-11-17 21:04:45 +0100

    - swagger package bug fix - installed specific version @5.2.1 - before  unable to resolve dependency tree

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthLoginDto {
   @IsEmail()
+  @ApiProperty({
+    type: String,
+    example: 'email@example.com',
+  })
   email: string;
 
   @IsString()
   @Length(6, 25)
+  @ApiProperty({
+    type: String,
+    example: 'pwdExample123',
+  })
   password: string;
 }
