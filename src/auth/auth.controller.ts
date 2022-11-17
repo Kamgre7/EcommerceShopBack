436f153c84ef5fda79a772d02e7798d1eb5fefde
kamgre7@gmail.com <kamgre7@gmail.com>
2022-11-17 21:05:31 +0100

    - swagger package bug fix - installed specific version @5.2.1 - before  unable to resolve dependency tree

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,20 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserObj } from '../decorators/user-obj.decorator';
 import { UserEntity } from '../user/entities/user.entity';
 import { LoginResponse, LogoutResponse } from '../types';
+import { ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
+  @ApiCreatedResponse({
+    description: 'User information object as response',
+    //type:
+  })
+  @ApiBadRequestResponse({
+    description: 'User cannot logged in. Try again',
+  })
   userLogin(
     @Body() authLoginDto: AuthLoginDto,
     @Res() res: Response,
