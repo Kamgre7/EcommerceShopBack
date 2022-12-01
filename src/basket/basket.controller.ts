0054736eb44423250e7f9a80ac632264f2ec6ad2
kamgre7@gmail.com <kamgre7@gmail.com>
2022-12-01 19:19:43 +0100

    Added basket swagger api, changed location of props file

--- src/basket/basket.controller.ts
+++ src/basket/basket.controller.ts
@@ -20,11 +20,44 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserObj } from '../decorators/user-obj.decorator';
 import { UserEntity } from '../user/entities/user.entity';
 import { UpdateBasketDto } from './dto/update-basket.dto';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import {
+  basketApiInformation,
+  basketApiMessage,
+  userApiMessage,
+} from 'src/utils/api-messages';
+import {
+  AddToBasketResponseProp,
+  BasketFilterResponseProp,
+  BasketUpdateResponseProp,
+  RemoveProductFromBasketProp,
+} from './props/basket.props';
 
+@ApiTags('Basket')
 @Controller('/basket')
 export class BasketController {
   constructor(private readonly basketService: BasketService) {}
 
+  @ApiCookieAuth('jwt')
+  @ApiCreatedResponse({
+    type: AddToBasketResponseProp,
+    description: basketApiMessage.addToBasket,
+  })
+  @ApiBody({
+    type: CreateBasketDto,
+    required: true,
+  })
+  @ApiUnauthorizedResponse({
+    description: userApiMessage.unauthorizedUser,
+  })
   @Post('/')
   @UseGuards(AuthGuard('jwt'))
   addToBasket(
@@ -34,6 +67,18 @@ export class BasketController {
     return this.basketService.addToBasket(createBasketDto, user);
   }
 
+  @ApiCookieAuth('jwt')
+  @ApiCreatedResponse({
+    type: BasketUpdateResponseProp,
+    description: basketApiMessage.changeItemQuantity,
+  })
+  @ApiBody({
+    type: UpdateBasketDto,
+    required: true,
+  })
+  @ApiUnauthorizedResponse({
+    description: userApiMessage.unauthorizedUser,
+  })
   @Patch('/')
   @UseGuards(AuthGuard('jwt'))
   changeItemQuantity(
@@ -43,18 +88,50 @@ export class BasketController {
     return this.basketService.changeItemQuantity(updateBasketDto, user);
   }
 
+  @ApiOkResponse({
+    description: 'Return array of user basket',
+    type: BasketFilterResponseProp,
+    isArray: true,
+  })
+  @ApiUnauthorizedResponse({
+    description: userApiMessage.unauthorizedUser,
+  })
   @Get('/')
   @UseGuards(AuthGuard('jwt'))
   showBasket(@UserObj() user: UserEntity): Promise<BasketFilterResponse[]> {
     return this.basketService.showBasket(user);
   }
 
+  @ApiCookieAuth('jwt')
+  @ApiOkResponse({
+    description: basketApiMessage.clearBasket,
+    type: RemoveProductFromBasketProp,
+  })
+  @ApiUnauthorizedResponse({
+    description: userApiMessage.unauthorizedUser,
+  })
   @Delete('/all')
   @UseGuards(AuthGuard('jwt'))
   clearBasket(@UserObj() user: UserEntity): Promise<RemoveProductFromBasket> {
     return this.basketService.clearBasket(user);
   }
 
+  @ApiCookieAuth('jwt')
+  @ApiOkResponse({
+    description: basketApiMessage.deleteBasket,
+    type: RemoveProductFromBasketProp,
+  })
+  @ApiUnauthorizedResponse({
+    description: userApiMessage.unauthorizedUser,
+  })
+  @ApiParam({
+    name: 'basketId',
+    type: String,
+    description: basketApiMessage.uniqueBasketId,
+    required: true,
+    format: 'uuid',
+    example: basketApiInformation.basketId,
+  })
   @Delete('/:basketId')
   @UseGuards(AuthGuard('jwt'))
   removeItem(
--- src/basket/dto/create-basket.dto.ts
+++ src/basket/dto/create-basket.dto.ts
@@ -1,11 +1,27 @@
 import { IsInt, IsUUID, Max, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  basketApiInformation,
+  productApiInformation,
+} from '../../utils/api-messages';
 
 export class CreateBasketDto {
   @IsUUID(4)
+  @ApiProperty({
+    type: String,
+    format: 'uuid',
+    example: productApiInformation.productId,
+  })
   productId: string;
 
   @IsInt()
   @Min(1)
   @Max(5000)
+  @ApiProperty({
+    type: Number,
+    minimum: 1,
+    maximum: 5000,
+    example: basketApiInformation.itemQuantity,
+  })
   quantity: number;
 }
