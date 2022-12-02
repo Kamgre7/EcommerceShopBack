93248edb85e334f5376417a56cbf072e37594694
kamgre7@gmail.com <kamgre7@gmail.com>
2022-12-02 20:51:33 +0100

    Added category swagger api proprt to category entity, added photo file response and single category

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -7,6 +7,8 @@ import {
 } from 'typeorm';
 import { ProductCategoryInterface } from '../../types';
 import { ProductEntity } from '../../product/entities/product.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { categoryApiInformation } from 'src/utils/api-messages';
 
 @Entity()
 export class ProductCategoryEntity
@@ -14,11 +16,20 @@ export class ProductCategoryEntity
   implements ProductCategoryInterface
 {
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty({
+    type: String,
+    format: 'uuid',
+    example: categoryApiInformation.categoryId,
+  })
   id: string;
 
   @Column({
     length: 25,
     unique: true,
+  })
+  @ApiProperty({
+    type: String,
+    example: categoryApiInformation.name,
   })
   name: string;
 
@@ -26,21 +37,37 @@ export class ProductCategoryEntity
     length: 1500,
     default: '',
   })
+  @ApiProperty({
+    type: String,
+    example: categoryApiInformation.description,
+  })
   description: string;
 
   @Column({
     default: null,
     nullable: true,
   })
+  @ApiProperty({
+    type: String,
+    example: categoryApiInformation.photoFileName,
+  })
   photoFileName: string;
 
   @Column({
     default: () => 'CURRENT_TIMESTAMP',
   })
+  @ApiProperty({
+    type: String,
+    example: categoryApiInformation.dateNow,
+  })
   createdAt: Date;
 
   @Column({
     default: () => 'CURRENT_TIMESTAMP',
+  })
+  @ApiProperty({
+    type: String,
+    example: categoryApiInformation.dateNow,
   })
   modifiedAt: Date;
 
